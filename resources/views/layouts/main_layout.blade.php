a67ffdd537df5363a1c2816f005c7d6610ed2a59
John Olipani <ampolanch@gmail.com>
2026-02-15 23:21:55 +0800

    fix: remove 'true' from vite css in main_layout for correct asset loading

--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -14,7 +14,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     
-    @vite('resources/css/main_style.css', true)
+    @vite('resources/css/main_style.css')
 </head>
 <body>
     <div class="logo-container">
