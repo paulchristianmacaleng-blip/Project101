2cbb4ba6e80530f6711ac3509d63b6fdd0226cb4
John Olipani <ampolanch@gmail.com>
2026-02-15 21:18:22 +0800

    fix: use secure asset loading for main_style.css to prevent mixed content

--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -14,7 +14,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     
-    @vite('resources/css/main_style.css')
+    @vite('resources/css/main_style.css', true)
 </head>
 <body>
     <div class="logo-container">
